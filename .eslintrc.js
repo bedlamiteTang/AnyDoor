1b663dbf6cce8fa7abbc8dbcc35fb59593bd76d4
bedlamite <bedlamite_tang@qq.com>
2018-07-08 04:07:52 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "script"
+    },
+    "rules": {
+        "no-console": [
+          "error",{
+            "allow": ["warn","error","info"]
+          }
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style":  [
+          "error",
+          "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
