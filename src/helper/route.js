92fc1d4d11f7036587f11c0fac7d32a43a4f6ed9
bedlamite <bedlamite_tang@qq.com>
2018-07-19 20:53:58 +0800

    add handlebars

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const promisify = require('util').promisify;
+const handlebars = require('handlebars');
+const path = require('path');
+const config = require('../config/defaultConfig');
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+
+const tplPath = path.join(__dirname,'../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = handlebars.compile(source.toString());
 
 module.exports = async function (req,res,filePath) {
     try {
@@ -14,11 +21,17 @@ module.exports = async function (req,res,filePath) {
             const files = await readdir(filePath);
             res.statusCode = 200;
             res.setHeader('Content-Type','text,plain');
-            res.end(files.join(','));
+            const data = {
+                title: path.basename(filePath),
+                dir: path.relative(config.root,filePath),
+                files
+            };
+            res.end(template(data));
         }
     } catch (error) {
         res.statusCode = 404;
         res.setHeader('Content-Type','text/plain');
+        res.write(error.toString()+'\n');
         res.end(`${filePath} is not a directory or file`);
     }
 };
